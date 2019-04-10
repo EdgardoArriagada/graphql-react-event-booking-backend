7c8221d53c12abd700ba5b04e7ba081ae2d41b3e
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-10 16:23:44 -0400

    create types for createEvent mutation adn evetns query

--- src/app.ts
+++ src/app.ts
@@ -23,12 +23,27 @@ app.use(
   '/graphql',
   graphqlHttp({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String
+        description: String!
+        price: Float!
+        date: String!
+      }
+
       type RootQuery {
-        events: [String!]!
+        events: [Event!]!
       }
 
       type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
       }
 
       schema {
