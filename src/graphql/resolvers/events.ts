32d280d6c946e3bb84b4e6606643f559bf5d24eb
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-15 15:23:44 -0400

    copy events resolver into a new file

---
+++ src/graphql/resolvers/events.ts
@@ -0,0 +1,41 @@
+import { Model } from 'mongoose'
+import { IEventDocument, IEventInput } from '../../interfaces/event.interface'
+import { standarizeEvent } from './merge'
+import { IUserDocument } from '../../interfaces/user.interface'
+
+const Event: Model<IEventDocument> = require('./../../models/event.model')
+const User: Model<IUserDocument> = require('./../../models/user.model')
+
+const testUserID = '5cb124f0c944961bb23937eb'
+
+module.exports = {
+  events: async (): Promise<IEventDocument['_doc'][]> => {
+    try {
+      const events: Array<IEventDocument> = await Event.find()
+      return events.map(
+        (event: IEventDocument): IEventDocument['_doc'] => {
+          return standarizeEvent(event)
+        }
+      )
+    } catch (e) {
+      throw e
+    }
+  },
+  createEvent: async (args: { eventInput: IEventInput }): Promise<IEventDocument['_doc']> => {
+    try {
+      const event = new Event({ ...args.eventInput, creator: testUserID })
+      const result = await event.save()
+      const createdEvent: IEventDocument['_doc'] = standarizeEvent(result)
+      const userCreator = await User.findById(testUserID)
+      if (!userCreator) {
+        throw new Error('User not found')
+      }
+      userCreator._doc.createdEvents.push(event.id)
+      await userCreator.save()
+
+      return createdEvent
+    } catch (e) {
+      throw e
+    }
+  }
+}
