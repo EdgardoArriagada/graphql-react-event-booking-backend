c9be79670a6f2bf0a5dfb542cb5e8b9f443989f5
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-15 15:23:32 -0400

    copy booking resolver into a new file

---
+++ src/graphql/resolvers/booking.ts
@@ -0,0 +1,45 @@
+export {} // hack to fix TSlint
+import { Model } from 'mongoose'
+import { IEventDocument } from '../../interfaces/event.interface'
+import { IBookingDocument } from '../../interfaces/booking.interface'
+import { standarizeBooking, standarizeEvent } from './merge'
+
+const Booking: Model<IBookingDocument> = require('./../../models/booking.model')
+const Event: Model<IEventDocument> = require('./../../models/event.model')
+
+const testUserID = '5cb124f0c944961bb23937eb'
+
+module.exports = {
+  bookings: async (): Promise<IBookingDocument['_doc'][]> => {
+    try {
+      const bookings = await Booking.find()
+      return bookings.map(
+        (booking: IBookingDocument): IBookingDocument['_doc'] => {
+          return standarizeBooking(booking)
+        }
+      )
+    } catch (e) {
+      throw e
+    }
+  },
+  bookEvent: async (args: { eventId: IEventDocument['_id'] }): Promise<IBookingDocument['_doc']> => {
+    try {
+      const fetchedEvent: IEventDocument = await Event.findOne({ _id: args.eventId })
+      const booking = new Booking({ user: testUserID, event: fetchedEvent._doc._id })
+      const result = await booking.save()
+      return standarizeBooking(result)
+    } catch (e) {
+      throw e
+    }
+  },
+  cancelBooking: async (args: { bookingId: IBookingDocument['_id'] }): Promise<IEventDocument['_doc']> => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate('event')
+      const event: IEventDocument['_doc'] = standarizeEvent(booking.event)
+      await Booking.deleteOne({ _id: args.bookingId })
+      return event
+    } catch (e) {
+      throw e
+    }
+  }
+}
