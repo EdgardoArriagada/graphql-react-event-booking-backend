74b16f42345d563809963952f3ce6785c3810384
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2019-04-16 11:24:30 -0400

    secure bookings by adding an authentication middleware

--- src/graphql/resolvers/booking.ts
+++ src/graphql/resolvers/booking.ts
@@ -4,11 +4,13 @@ import { IBookingDocument } from '../../interfaces/booking.interface'
 import { standarizeBooking, standarizeEvent } from './utils'
 import { Booking } from '../../models/booking.model'
 import { Event } from '../../models/event.model'
-
-const testUserID = '5cb124f0c944961bb23937eb'
+import { IAppRequest } from '../../middleware/is-auth.middleware'
 
 module.exports = {
-  bookings: async (): Promise<IBookingDocument['_doc'][]> => {
+  bookings: async (req: IAppRequest): Promise<IBookingDocument['_doc'][]> => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated')
+    }
     try {
       const bookings = await Booking.find()
       return bookings.map(
@@ -20,17 +22,26 @@ module.exports = {
       throw e
     }
   },
-  bookEvent: async (args: { eventId: IEventDocument['_id'] }): Promise<IBookingDocument['_doc']> => {
+  bookEvent: async (args: { eventId: IEventDocument['_id'] }, req: IAppRequest): Promise<IBookingDocument['_doc']> => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated')
+    }
     try {
       const fetchedEvent: IEventDocument = await Event.findOne({ _id: args.eventId })
-      const booking = new Booking({ user: testUserID, event: fetchedEvent._doc._id })
+      const booking = new Booking({ user: req.userId, event: fetchedEvent._doc._id })
       const result = await booking.save()
       return standarizeBooking(result)
     } catch (e) {
       throw e
     }
   },
-  cancelBooking: async (args: { bookingId: IBookingDocument['_id'] }): Promise<IEventDocument['_doc']> => {
+  cancelBooking: async (
+    args: { bookingId: IBookingDocument['_id'] },
+    req: IAppRequest
+  ): Promise<IEventDocument['_doc']> => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated')
+    }
     try {
       const booking = await Booking.findById(args.bookingId).populate('event')
       const event: IEventDocument['_doc'] = standarizeEvent(booking.event)
